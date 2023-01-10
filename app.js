1b47d1b0284fcce1d26d7ca5ac4e55945246e3d3
aaronchellaya <aaron@pingalasoftware.com>
2023-01-10 18:22:27 +0530

    path changed

--- app.js
+++ app.js
@@ -1,97 +1,112 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const path = require('path')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 3002;
+const path = require("path");
+const cors = require("cors");
 
-app.use(cors())
-var fs = require('fs')
+app.use(cors());
+var penDrive = "";
 
-// app.get('/', (req, res, next) => {
-//   console.log(__dirname)
-//   let filePath = path.join(
-//     __dirname,
-//     '..',
-//     '..',
-//     '..',
-//     '..',
-//     'media',
-//     'aaron25',
-//     'MALL'
-//   )
-//   // res.send(filePath);
+// const fs = require("fs");
+// let textFilePath = path.join(__dirname, "..", "test.txt");
+// fs.readFile(textFilePath, (err, textFile) => {
+//   console.log(__dirname);
+//   if (err) throw err;
+//   console.log(textFile.toString());
+//   if (textFile.includes("media/aaron25")) console.log("pen drive is there");
+//   else console.log("pen drive is no there");
+//   penDrive = textFile.toString().split("/media/aaron25/")[1];
+//   penDrive = penDrive.split(`\n`)[0];
+//   console.log(penDrive);
+// });
+// console.log(penDrive);
 
-//   res.sendFile('config.txt', { root: filePath }, function (err) {
-//     if (err) {
-//       next({ error: true })
-//     } else {
-//       next()
-//     }
-//   })
+app.get("/", (req, res, next) => {
+  console.log(__dirname);
+  // console.log(penDrive == "MALL", penDrive, typeof penDrive);
+  let filePath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "..",
+    "media",
+    "aaron25",
+    "MALL"
+  );
+  // res.send(filePath);
 
-//   // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
-//   //   if (!err) {
-//   //     console.log("received data: " + data);
-//   //     res.end();
-//   //   } else {
-//   //     console.log(err);
-//   //   }
-//   // });
-// })
-// app.get('/:filename', (req, res, next) => {
-//   console.log(__dirname)
-//   let filePath = path.join(
-//     __dirname,
-//     '..',
-//     '..',
-//     '..',
-//     '..',
-//     'media',
-//     'aaron25',
-//     'MALL'
-//   )
-//   // res.send(filePath);
+  res.sendFile("config.txt", { root: filePath }, function (err) {
+    if (err) {
+      next({ error: true });
+    } else {
+      next();
+    }
+  });
 
-//   res.sendFile(req.params.filename, { root: filePath }, function (err) {
-//     if (err) {
-//       next({ error: true })
-//     } else {
-//       next()
-//     }
-//   })
+  // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+  //   if (!err) {
+  //     console.log("received data: " + data);
+  //     res.end();
+  //   } else {
+  //     console.log(err);
+  //   }
+  // });
+});
+app.get("/:filename", (req, res, next) => {
+  console.log(__dirname);
+  let filePath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "..",
+    "media",
+    "aaron25",
+    "MALL"
+  );
+  // res.send(filePath);
 
-//   // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
-//   //   if (!err) {
-//   //     console.log("received data: " + data);
-//   //     res.end();
-//   //   } else {
-//   //     console.log(err);
-//   //   }
-//   // });
-// })
+  res.sendFile(req.params.filename, { root: filePath }, function (err) {
+    if (err) {
+      next({ error: true });
+    } else {
+      next();
+    }
+  });
 
-var detected = false
-console.log(detected)
+  // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+  //   if (!err) {
+  //     console.log("received data: " + data);
+  //     res.end();
+  //   } else {
+  //     console.log(err);
+  //   }
+  // });
+});
 
-app.post('/detected', (req, res, next) => {
-  detected = true
-  console.log(detected)
-  res.send('done')
-})
-app.post('/notdetected', (req, res, next) => {
-  detected = false
-  console.log(detected)
-  res.send('done')
-})
+var detected = false;
+console.log(detected);
 
-app.post('/blackoutscrenn', (req, res, next) => {
-  console.log(detected)
-  res.send(detected)
-})
+app.post("/detected", (req, res, next) => {
+  detected = true;
+  console.log(detected);
+  res.send("done");
+});
+app.post("/notdetected", (req, res, next) => {
+  detected = false;
+  console.log(detected);
+  res.send("done");
+});
+
+app.post("/blackoutscrenn", (req, res, next) => {
+  console.log(detected);
+  res.send(detected);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 // const axios = require('axios')
 // async function tenthque() {
 //   try {
